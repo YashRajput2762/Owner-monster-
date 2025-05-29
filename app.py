e3389907dbf9b56f7f79526bf6d35e9b34009ac2
YashRajput2762 <ythunder491@gmail.com>
2025-05-29 11:52:59 +0530

    Delete app.py

--- app.py
+++
@@ -1,6 +0,0 @@
-app = Flask(__name__)
-  <script>
-    async function checkToken() {
-      const accessToken = document.getElementById("accessToken").value;
-      const resultDiv = document.getElementById("result");
-      resultDiv.innerHTML = "";
